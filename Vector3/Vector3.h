2c9de072c8c1e469dff285482a9bf8721097c546
MrIllas <44169046+MrIllas@users.noreply.github.com>
2021-09-25 17:19:25 +0200

    UPDATE

--- Vector3/Vector3.h
+++ Vector3/Vector3.h
@@ -1,17 +1,95 @@
 #pragma once
+
+#ifndef __VECTOR3_H__
+#define __VECTOR3_H__
+
+#include <math.h>
+
+template <typename T>
 class Vector3
 {
-
-private:
-	float x;
-	float y;
-	float z;
-
-protected:
-
 public:
 
-	Vector3();
-	Vector3(float x, float y, float z);
+	T x, y, z;
+
+	Vector3<T>() {};
+	Vector3<T>(const Vector3& vec) { 
+		this->x = vec.x;
+		this->y = vec.y;
+		this->z = vec.z;
+	};
+	Vector3<T>(T x, T y, T z) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	};
+
+	void Zero() {
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+
+	bool isZero() const {
+		return (x == 0 && y == 0 && z == 0);
+	}
+
+	T DistanceTo(Vector3 vec) {
+		T fx = this->x - vec.x;
+		T fy = this->y - vec.y;
+		T fz = this->z - vec.z;
+
+		return (T) sqrt((float) (fx*fx) + (fy*fy) + (fz*fz));
+	}
+
+	void Normalize() {
+		this->x = this->x / this->x;
+		this->y = this->y / this->y;
+		this->z = this->z / this->z;
+	}
+
+
+	//OPERATORS
+	Vector3 operator+(Vector3 &vec) {
+		Vector3 toReturn;
+		toReturn.x = x + vec.x;
+		toReturn.y = y + vec.y;
+		toReturn.z = z + vec.z;
+
+		return toReturn;
+	};
+	Vector3 operator-(Vector3 &vec) {
+		Vector3 toReturn;
+		toReturn.x = x - vec.x;
+		toReturn.y = y - vec.y;
+		toReturn.z = z - vec.z;
+
+		return toReturn;
+	};
+	Vector3 operator+=(Vector3 &vec) {
+		x += vec.x;
+		y += vec.y;
+		z += vec.z;
+
+		return this;
+	};
+	Vector3 operator-=(Vector3 &vec) {
+		x -= vec.x;
+		y -= vec.y;
+		z -= vec.z;
+
+		return this;
+	};
+	
+	void operator= (Vector3 vec) {
+		this->x = vec.x;
+		this->y = vec.y;
+		this->z = vec.z;
+	};
+	
+	bool operator==(Vector3 vec) {
+		return (x == vec.x && y == vec.y && z == vec.z);
+	};
 };
 
+#endif
--- Vector3/main.cpp
+++ Vector3/main.cpp
@@ -1,1 +1,18 @@
 #include "Vector3.h"
+#include <iostream>
+
+int main() {
+
+	Vector3<float> vec1 = Vector3<float>(2.0f, 3.0f, 4.0f);
+	Vector3<int> vec2;
+	Vector3<double> vec3;
+
+	vec2.Zero();
+	vec3.Normalize();
+
+	std::cout << "Vec1-> " << vec1.x << ", " << vec1.y << ", " << vec1.z << std::endl;
+	std::cout << "Vec2-> " << vec2.x << ", " << vec2.y << ", " << vec2.z << std::endl;
+	std::cout << "Vec3-> " << vec3.x << ", " << vec3.y << ", " << vec3.z << std::endl;
+
+	return 0;
+}
